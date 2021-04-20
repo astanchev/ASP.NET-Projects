0765c06822fa2e871918ae46be9745822ca4995b
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-04-20 11:27:41 +0300

    Initial commit

---
+++ Shop/Shop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace Shop.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
