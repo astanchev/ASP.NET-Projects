a9aa689b96b6fcc61e9012c2f554ce2b07943018
Атанас Станчев <mr_Stanchev@hotmail.com>
2021-04-21 19:06:51 +0300

    Add Username to Register

--- Online Shop/OnlineShop.Common/GlobalConstants.cs
+++ Online Shop/OnlineShop.Common/GlobalConstants.cs
@@ -5,5 +5,11 @@
         public const string SystemName = "OnlineShop";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string UsernameErrorRegex = "The {0} must have only letters and numbers.";
+
+        public const string TextError = "The {0} must be at least {2} and at max {1} characters long.";
+
+        public const string UserExists = "User with this name exists!";
     }
 }
